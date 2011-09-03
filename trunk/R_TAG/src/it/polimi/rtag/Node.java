4c1b1410c7d104386eaad48892a7ed8c8a46e8c2
panteha.s@gmail.com <panteha.s@gmail.com@ac02f2be-dc56-3d54-cb1e-a53a8154948c>
2011-09-03 22:39:21 +0000

    adding lights @ lib adding new features to the node

--- trunk/R_TAG/src/it/polimi/rtag/Node.java
+++ trunk/R_TAG/src/it/polimi/rtag/Node.java
@@ -1,5 +1,6 @@
 package it.polimi.rtag;
 
+import it.polimi.rtag.messaging.MessageReport;
 import polimi.reds.NodeDescriptor;
 
 public class Node {
@@ -13,5 +14,21 @@ public class Node {
 	public void setCurrentDescriptor(NodeDescriptor currentDescriptor) {
 		this.currentDescriptor = currentDescriptor;
 	}
+	
+	/**
+	 * Attempts to join a group and if it does not exist creates a new one
+	 */
+	public boolean joinOrCreate(Tuple groupDescription) {
+		// TODO fix tuple with lights
+		throw new AssertionError("Not yet implemented error.");
+	}
 
+	public MessageReport sendMessage(Message msg, NodeDescriptor... recipients) {
+		throw new AssertionError("Not yet implemented error.");
+	}
+	
+	
+	protected void checkLeaderStatus {
+		throw new AssertionError("Not yet implemented error.");
+	}
 }
---
+++ trunk/R_TAG/src/it/polimi/rtag/messaging/MessageType.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package it.polimi.rtag.messaging;
+
+/**
+ * @author panteha
+ *
+ */
+public enum MessageType {
+	Direct, Multicast, Singlecast, Broadcast
+}
