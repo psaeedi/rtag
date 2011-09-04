dcc47575be7e143ffd36b5286e9273a5f860240e
panteha.s@gmail.com <panteha.s@gmail.com@ac02f2be-dc56-3d54-cb1e-a53a8154948c>
2011-09-04 15:28:43 +0000

    adding to Node.java class
    GroupFilter

--- trunk/R_TAG/src/it/polimi/rtag/Node.java
+++ trunk/R_TAG/src/it/polimi/rtag/Node.java
@@ -1,30 +1,91 @@
 package it.polimi.rtag;
 
 import java.io.Serializable;
+import java.util.TreeSet;
+import java.util.logging.Logger;
 
 import lights.Tuple;
 
 import it.polimi.rtag.messaging.MessageReport;
 import polimi.reds.Filter;
 import polimi.reds.Message;
+import polimi.reds.MessageID;
 import polimi.reds.NodeDescriptor;
 import polimi.reds.Reply;
+import polimi.reds.broker.overlay.GenericOverlay;
 import polimi.reds.broker.overlay.Overlay;
+import polimi.reds.broker.overlay.SimpleTopologyManager;
+import polimi.reds.broker.overlay.TCPTransport;
+import polimi.reds.broker.overlay.TopologyManager;
+import polimi.reds.broker.overlay.Transport;
+import polimi.reds.broker.routing.GenericTable;
+import polimi.reds.broker.routing.HashReplyTable;
+import polimi.reds.broker.routing.ImmediateForwardReplyManager;
+import polimi.reds.broker.routing.ReplyManager;
 import polimi.reds.broker.routing.ReplyTable;
 import polimi.reds.broker.routing.Router;
+import polimi.reds.broker.routing.RoutingStrategy;
+import polimi.reds.broker.routing.SubscriptionForwardingRoutingStrategy;
 import polimi.reds.broker.routing.SubscriptionTable;
+
+
+
 
 public class Node implements Router{
 
     public NodeDescriptor currentDescriptor;
     private GroupingStrategy strategy;
     private Overlay overlay;
+    private RoutingStrategy routingStrategy;
+	private ReplyManager replyManager;
+	SubscriptionTable subscriptionTable;
+	ReplyTable replyTable;
+	private Object nodeReply;
     
-	@Override
-	public void forwardReply(Reply arg0) {
-		// TODO Auto-generated method stub
+	public Node(String address, int port) {
+		/**
+		 * This interface is the "core" of a REDS broker. Classes that implement this interface constitutes
+		 * the main component of a REDS broker. It is registered to the <code>Overlay</code> to receive
+		 * messages coming from the other nodes of the network. In most cases it delegates its main
+		 * functionalities to the other components that compose a REDS broker: namely, the
+		 * <code>RoutingStrategy</code> and the <code>ReplyManager</code>. It holds the common data
+		 * structure managed by those components: the <code>SubscriptionTable</code> and the
+		 * <code>ReplyTable</code>.
+		 */
 		
+		// TODO implement a new routing topology manager
+		TopologyManager topologyManager = new SimpleTopologyManager();
+		Transport transport = new TCPTransport(port);
+		
+		overlay = new GenericOverlay(topologyManager, transport);
+		
+		// TODO implement a new routing strategy
+		routingStrategy = new SubscriptionForwardingRoutingStrategy();
+		
+		// TODO implement a new reply strategy
+		replyManager = new ImmediateForwardReplyManager();
+		
+		subscriptionTable = new GenericTable();
+		
+		replyTable = new HashReplyTable();
 	}
+    
+    
+	  /**
+	   * Forwards the given reply to to its sender. According to the informations contained into the
+	   * local reply table the reply will be sent to a specific neighbor or dropped if no information
+	   * about its sender is contained in the reply table.
+	   * <p>
+	   * Note that reconfigurators assume that the implementation of this method is synchronized on the
+	   * router object, so that the reconfigurator can acquire the lock when it needs to make sure that
+	   * no router operations are executed concurrently with reconfiguration actions.
+	   * 
+	   * @param reply the reply to be sent.
+	   */
+	@Override
+	public void forwardReply(Reply reply) {
+		replyManager.forwardReply(reply);
+	  }
 
 	@Override
 	public Overlay getOverlay() {
@@ -33,14 +94,12 @@ public class Node implements Router{
 
 	@Override
 	public ReplyTable getReplyTable() {
-		// TODO Auto-generated method stub
-		return null;
+		return replyTable;
 	}
 
 	@Override
 	public SubscriptionTable getSubscriptionTable() {
-		// TODO Auto-generated method stub
-		return null;
+		return subscriptionTable;
 	}
 
 	@Override
@@ -55,21 +114,51 @@ public class Node implements Router{
 	}
 
 	@Override
-	public void subscribe(NodeDescriptor arg0, Filter arg1) {
-		// TODO Auto-generated method stub
-		
+	public void subscribe(NodeDescriptor node, Filter filter) {
+		// Check if the filter represents a group
+		try {
+			GroupFilter groupFilter = GroupFilter.createFromFilter(filter);
+			// Check if this node is the right group leader
+			GroupDescriptor groupDescriptor = groupFilter.getDescriptor();
+			if (groupDescriptor.isLeader(currentDescriptor)) {
+				// TODO Add node to the group and notify all the other followers
+			} else {
+				// TODO decide what to do if the current node is not the 
+				// group leader. We could reply with the leader address if known.
+				// Otherwhise we could simply send an error message.
+			}
+			
+		} catch(IllegalArgumentException ex) {
+			// Not a valid group filter: return
+			return;
+		}
 	}
 
 	@Override
-	public void unsubscribe(NodeDescriptor arg0, Filter arg1) {
-		// TODO Auto-generated method stub
-		
+	public void unsubscribe(NodeDescriptor node, Filter filter) {
+		// Check if the filter represents a group
+		try {
+			GroupFilter groupFilter = GroupFilter.createFromFilter(filter);
+			// Check if this node is the right group leader
+			GroupDescriptor groupDescriptor = groupFilter.getDescriptor();
+			if (groupDescriptor.isLeader(currentDescriptor)) {
+				// TODO Remove node to the group and notify all the other followers
+			} else {
+				// TODO decide what to do if the current node is not the 
+				// group leader. We could reply with the leader address if known.
+				// Otherwhise we could simply send an error message.
+			}
+			
+		} catch(IllegalArgumentException ex) {
+			// Not a valid group filter: return
+			return;
+		}
 	}
 
 	@Override
-	public void unsubscribeAll(NodeDescriptor arg0) {
-		// TODO Auto-generated method stub
-		
+	public void unsubscribeAll(NodeDescriptor node) {
+		// Invoked before a node is quitting or if a node has collapsed
+		// TODO the node has to be removed from all the groups
 	}
 
 	@Override
