f7dc0f1464b82f7cc471cd0ac61059126f2eb334
panteha.s@gmail.com <panteha.s@gmail.com@ac02f2be-dc56-3d54-cb1e-a53a8154948c>
2011-09-11 12:33:40 +0000

    GroupLeaderCommandAck:

--- R_TAG/src/it/polimi/rtag/messaging/GroupLeaderCommandAck.java
+++ R_TAG/src/it/polimi/rtag/messaging/GroupLeaderCommandAck.java
@@ -10,6 +10,27 @@ import polimi.reds.Message;
  *
  */
 public class GroupLeaderCommandAck extends Message {
+	
+	
+	/**
+	 * The {@link GroupLeaderCommandAck} message was received correctly 
+	 * and the sender fulfilled the command.
+	 */
+	public static final String OK = "OK";
+	
+	/**
+	 * The message was received but the recipient was not that group follower
+	 * or no longer the group follower.</p>
+	 * and therefore it is not allowed to perform the required action.
+	 */
+	public static final String NOT_GROUP_FOLLOWER = "NGF";
+	
+	/**
+	 * The Follower has declined the command. 
+	 * In this case the follower should leave the group. 
+	 */
+	public static final String KO = "KO";
+	
 
 	/**
 	 * 
